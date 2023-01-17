caf0a2082d54fabccccbc205016074302f53837b
Pratik <pratik.pj48@gmail.com>
2023-01-17 15:12:30 +0530

    not able to config couchbase via application.properties

--- src/main/java/com/pratik/www/employee/db/repository/EmployeeEntityRepository.java
+++ src/main/java/com/pratik/www/employee/db/repository/EmployeeEntityRepository.java
@@ -1,9 +1,13 @@
 package com.pratik.www.employee.db.repository;
 
 import com.pratik.www.employee.db.entity.EmployeeEntity;
+import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
+import org.springframework.data.couchbase.core.query.ViewIndexed;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+//@N1qlPrimaryIndexed
+//@ViewIndexed(designDoc = "employee", viewName = "all")
 public interface EmployeeEntityRepository extends CouchbaseRepository<EmployeeEntity,String> {
 }
